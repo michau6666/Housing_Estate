5050b36cae8a644e7b0d438977f510aa469742e6
Michal Bernát <113421230+michau6666@users.noreply.github.com>
2023-01-19 07:51:13 +0100

    Update Person.cs

--- housingestate_2/Person.cs
+++ housingestate_2/Person.cs
@@ -6,26 +6,46 @@ using System.Threading.Tasks;
 
 namespace HousingEstate
 {
-    class Program
+    class Person
     {
-        static void Main(string[] args)
+        private string name;
+        private string surname;
+        private int age;
+        private Flat xFlat;
+        public Flat XFlat
         {
-            Person FeroSpara = new Person("Fero", "Spara", 64);
-            Console.WriteLine(FeroSpara.ToString());
-            Person TetaJurcova = new Person("teta", "jurcova", 26);
-            Person RisovaSestra = new Person("risova", "sestra", 40);
-            Person Riso = new Person("riso", "jurco", 17);
-
-            Flat RisovByt = new Flat(1325, 50, 2);
-            RisovByt.AddInhabitant(TetaJurcova);
-            RisovByt.AddInhabitant(RisovaSestra);
-            RisovByt.AddInhabitant(Riso);
-            Console.WriteLine(RisovByt.ToString());
-            foreach(var inhabitant in RisovByt.inhabitants)
-            {
-                Console.WriteLine(inhabitant.ToString());
-            }
-
+            get { return xFlat; }
+            set { this.xFlat = value; }
         }
+
+        public Person(string name, string surname, int age)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.age = age;
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Surname
+        {
+            get { return surname; }
+            set { surname = value; }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+        public override string ToString()
+        {
+            return string.Format($"name: { name }\n surname: { surname}\n age: {age}");
+        }
+
     }
 }
